9bb4caee51b94abbd896cc152dc7ead4beec5ea6
Borna S <bornasepic98@gmail.com>
2019-06-12 19:05:42 +0200

    :sparkles: Players created

--- pong.js
+++ pong.js
@@ -7,7 +7,52 @@ let maxHeight = 400;
 canvas.height = maxHeight;
 canvas.width = maxWidth;
 
+c.fillStyle = "whitesmoke";
+c.strokeStyle = "whitesmoke";
+
+class Player {
+    constructor(playerNumber) {
+        this.height = 50;
+        this.width = 10;
+        this.velocity = 2;
+        this.xCord = playerNumber === 1 ? 10 : 780;
+        this.yCord = 0;
+    }
+
+    moveUp() {
+        if (this.yCord <= 0) {
+            return
+        }
+
+        this.yCord = this.yCord - this.velocity;
+    }
+
+    moveDown() {
+        if (this.yCord > maxHeight) {
+            return
+        }
+
+        this.yCord = this.yCord + this.velocity;
+    }
+
+    draw() {
+        c.beginPath();
+        c.rect(this.xCord, this.yCord, this.width, this.height);
+        c.fill();   
+        c.stroke();
+    }
+}
+
+const playerOne = new Player(1);
+const playerTwo = new Player(2);
+
+
 function runGame() {
+    c.clearRect(0, 0, maxWidth, maxHeight);
+    playerOne.draw();
+    playerTwo.draw();
+    
+
     requestAnimationFrame(function() {
         runGame();
     });
