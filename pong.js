02fb21fb3adacd84d21f1de857a594323964bba4
Borna S <bornasepic98@gmail.com>
2019-06-12 23:13:14 +0200

    :boom: Added ball / collision

--- pong.js
+++ pong.js
@@ -10,9 +10,67 @@ pongCanvas.width = pongMaxWidth;
 pongC.fillStyle = "whitesmoke";
 pongC.strokeStyle = "whitesmoke";
 
+function collisionDetected() {
+    const ballCordinates = {x: ball.xCord, y: ball.yCord};
+    const playerCordinates = [{x: playerOne.xCord, y: playerOne.yCord}, {x: playerTwo.xCord, y: playerTwo.yCord}];
+
+    if(ballCordinates.y > playerCordinates[1].y - 10 && ballCordinates.y < playerCordinates[1].y + 60 && ballCordinates.x > pongMaxWidth / 2) {
+        return true
+    }
+
+    if(ballCordinates.y > playerCordinates[0].y - 10 && ballCordinates.y < playerCordinates[0].y + 60) {
+        return true
+    }
+
+    return false;
+}
+
+
 class Game {
     constructor() {
+        this.score = {playerOne: 0, playerTwo: 0}
         this.movement = [];
+        this.gameActive = true;
+    }
+
+    stopGame() {
+        this.gameActive = false;
+    }
+}
+
+class Ball {
+    constructor() {
+        this.xCord = pongMaxWidth / 2;
+        this.yCord = pongMaxHeight / 2;
+        this.r = 15;
+        this.xVel = 3;
+        this.yVel = 3;
+    }
+
+    move() {
+        if(this.xCord < 26 || this.xCord > 774) {
+            if(!collisionDetected()) {
+                game.stopGame();
+                return;
+            }
+            this.xVel = this.xVel * -1;    
+        }
+
+        if(this.yCord <= this.r || this.yCord > pongMaxHeight - this.r) {
+            this.yVel = this.yVel * -1;
+        }
+
+        this.xCord = this.xCord + this.xVel;
+        this.yCord = this.yCord + this.yVel;
+        
+    }
+
+    draw() {
+        pongC.beginPath();
+        pongC.arc(this.xCord, this.yCord, this.r, 0, 2 * Math.PI, false);
+        pongC.fillStyle = "whitesmoke";
+        pongC.fill();
+        pongC.stroke();
     }
 }
 
@@ -22,7 +80,7 @@ class Player {
         this.width = 10;
         this.velocity = 2;
         this.xCord = playerNumber === 1 ? 2 : 788;
-        this.yCord = 0;
+        this.yCord = pongMaxHeight / 2 - 25;
     }
 
     moveUp() {
@@ -47,17 +105,25 @@ class Player {
         pongC.stroke();
     }
 }
+
 const game = new Game();
+const ball = new Ball();
 const playerOne = new Player(1);
 const playerTwo = new Player(2);
 
 const allowedMoves = [87, 83, 38, 40];
 
 function runGame() {
+    if(!game.gameActive) {
+        return;
+    }
+
     pongC.clearRect(0, 0, pongMaxWidth, pongMaxHeight);
+    ball.draw();
     playerOne.draw();
     playerTwo.draw();
-    game.movement.map(move => playersMove(move))
+    game.movement.map(move => playersMove(move));
+    ball.move();
 
     requestAnimationFrame(function() {
         runGame();
