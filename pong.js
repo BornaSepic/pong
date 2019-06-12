404b4d52dfb3625331d8398fa85128de2cdd0782
Borna S <bornasepic98@gmail.com>
2019-06-12 20:28:41 +0200

    :rocket: Movement initiated

--- pong.js
+++ pong.js
@@ -10,6 +10,12 @@ canvas.width = maxWidth;
 c.fillStyle = "whitesmoke";
 c.strokeStyle = "whitesmoke";
 
+class Game {
+    constructor() {
+        this.movement = [];
+    }
+}
+
 class Player {
     constructor(playerNumber) {
         this.height = 50;
@@ -23,12 +29,11 @@ class Player {
         if (this.yCord <= 0) {
             return
         }
-
         this.yCord = this.yCord - this.velocity;
     }
 
     moveDown() {
-        if (this.yCord > maxHeight) {
+        if (this.yCord > maxHeight - 50) {
             return
         }
 
@@ -42,20 +47,59 @@ class Player {
         c.stroke();
     }
 }
-
+const game = new Game();
 const playerOne = new Player(1);
 const playerTwo = new Player(2);
 
+const allowedMoves = [87, 83, 38, 40];
 
 function runGame() {
     c.clearRect(0, 0, maxWidth, maxHeight);
     playerOne.draw();
     playerTwo.draw();
-    
+    game.movement.map(move => playersMove(move))
 
     requestAnimationFrame(function() {
         runGame();
     });
 }
+
+function playersMove(keyCode) {
+    switch(keyCode) {
+        case(87): {
+            playerOne.moveUp();
+            break;
+        }
+        case(83): {
+            playerOne.moveDown();
+            break;
+        }
+        case(38): {
+            playerTwo.moveUp();
+            break;
+        }
+        case(40): {
+            playerTwo.moveDown();
+            break;
+        }
+        default: {
+            return
+        }
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if(!allowedMoves.includes(e.keyCode) || game.movement.includes(e.keyCode)) {
+        return;
+    }
+    game.movement.push(e.keyCode);
+});
+
+document.addEventListener('keyup', (e) => {
+    if(!allowedMoves.includes(e.keyCode)) {
+        return;
+    }
+    game.movement = game.movement.filter(move => move !== e.keyCode);
+});
 
 runGame();
